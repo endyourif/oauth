66700f7bbb67b286c0ec877d9c63741e6768b8e6
Jamie Munro <info@endyourif.com>
2012-11-23 16:39:00 -0500

    constant name fix

--- includes/config.php
+++ includes/config.php
@@ -4,6 +4,6 @@ session_start();
 // Create constants for database connection
 define('DB_TYPE', 'MySQLi');
 define('DB_NAME', 'oauth');
-define('DB_SERVER', 'localhost');
+define('DB_HOST', 'localhost');
 define('DB_USERNAME', 'oauth');
 define('DB_PASSWORD', 'oauth99');
