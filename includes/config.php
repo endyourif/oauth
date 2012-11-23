7fa0994de055d4c17578721c7ced053d65506844
Jamie Munro <info@endyourif.com>
2012-11-23 16:38:13 -0500

    constants updates

--- includes/config.php
+++ includes/config.php
@@ -2,8 +2,8 @@
 session_start();
 
 // Create constants for database connection
-define(DB_TYPE, 'MySQLi');
-define(DB_NAME, 'oauth');
-define(DB_SERVER, 'localhost');
-define(DB_USERNAME, 'oauth');
-define(DB_PASSWORD, 'oauth99');
+define('DB_TYPE', 'MySQLi');
+define('DB_NAME', 'oauth');
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'oauth');
+define('DB_PASSWORD', 'oauth99');
